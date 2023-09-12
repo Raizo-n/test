11792bc9f1ffd910c89fbc76aaacbb61a747515a
Raizo Nakada <84226538+Raizo-n@users.noreply.github.com>
2023-09-12 23:30:09 +0000

    feat: delete a task from Undone Todos

--- src/index.mjs
+++ src/index.mjs
@@ -13,7 +13,23 @@ const onClickAdd = () => {
     const li = document.createElement('li');
     li.innerText = inputText;
 
+    // Add done and delete button to each created task
+    const doneBtn = document.createElement('button');
+    doneBtn.innerText = 'Done'
+    doneBtn.addEventListener('click', () => {
+        alert('yoo')
+    })
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.addEventListener('click', () => {
+        const deleteTarget = deleteBtn.parentNode;
+        document.getElementById('incomplete-list').removeChild(deleteTarget);
+    })
+    
     div.appendChild(li);
+    div.appendChild(doneBtn);
+    div.appendChild(deleteBtn);
     document.getElementById('incomplete-list').appendChild(div);
 }
 
