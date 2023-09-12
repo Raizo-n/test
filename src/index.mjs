f852f7ddececf575792ca6eed85e34080c9820ba
Raizo Nakada <84226538+Raizo-n@users.noreply.github.com>
2023-09-12 23:08:48 +0000

    feat: onClickAdd for adding a task to Undone Todos

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,22 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+    // get the value from the input and clear the input box after adding a task
+    const inputText = document.getElementById('input-task').value;
+    document.getElementById('input-task').value = '';
+
+    // Add div tag
+    const div = document.createElement('div');
+    div.className = 'flex'
+
+    // Add li tag
+    const li = document.createElement('li');
+    li.innerText = inputText;
+
+    div.appendChild(li);
+    document.getElementById('incomplete-list').appendChild(div);
+}
+
+document
+    .getElementById('add-button')
+    .addEventListener('click', () => onClickAdd())
